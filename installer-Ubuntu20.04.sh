2a7edf33efcda54d5c36769c1c9a9cf0eaf02670
唯岸 雪 <64316110+YukiYuigishi@users.noreply.github.com>
2021-04-28 12:04:10 +0900

    Update installer-Ubuntu20.04.sh

--- installer-Ubuntu20.04.sh
+++ installer-Ubuntu20.04.sh
@@ -8,7 +8,7 @@ done << FILE
 $DATA 
 FILE
 cd $HOME
-apt update && apt upgrade -y && apt install -y git clangd  ninja-build gettext libtool curl libtool-bin autoconf automake cmake g++ pkg-config unzip
+apt update && apt upgrade -y && apt install -y git clangd-10  ninja-build gettext libtool curl libtool-bin autoconf automake cmake g++ pkg-config unzip
 
 curl -sL install-node.now.sh/lts | bash
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installdein.sh

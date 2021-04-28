4afeedd5e33623a75d728738b96969ffdc17341c
唯岸 雪 <64316110+YukiYuigishi@users.noreply.github.com>
2021-04-28 11:09:37 +0900

    Create installer-Ubuntu20.04.sh
    自動的に環境構築を可能にする

---
+++ installer-Ubuntu20.04.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Install Yuki environment"
+DATA=`cat /etc/issue`
+while read line
+do
+   echo $line
+done << FILE
+$DATA 
+FILE
+
+apt update && apt upgrade -y && apt install -y git clangd  ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+
+curl -sL install-node.now.sh/lts | bash
+
+mkdir ~/tmp
+mkdir -p ~/.cache/dein
+mkdir ~/.config
+
+###build neovim
+echo "start neovim build"
+
+
+git clone https://github.com/neovim/neovim ~/tmp/neovim
+
+cd ~/tmp/neovim
+make CMAKE_BUILD_TYPE=Release
+mkdir -p $HOME/local/nvim
+make CMAKE_INSTALL_PREFIX=$HOME/local/nvim install
+##PATH
+#export PATH="$HOME/local/nvim:$PATH"
+git clone https://github.com/YukiYuigishi/dotfiles $HOME/tmp/dotfiles
+echo "install dotfiles"
+ln -s $HOME/tmp/dotfiles/nvim $HOME/.config/nvim
+

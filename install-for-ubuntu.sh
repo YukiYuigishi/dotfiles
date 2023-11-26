4caf5af11bc7fb36350f19482156c641bff463c3
Yuki Yuigishi <me@yuigishi.dev>
2023-11-27 03:09:45 +0900

    chore:Update install-for-ubuntu.sh

--- install-for-ubuntu.sh
+++ install-for-ubuntu.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-echo "download dotfiles"
-git clone https://github.com/YukiYuigishi/dotfiles.git ~/.dotfiles
+# install dependencies
 sudo apt-get update && sudo apt get upgrade -y
 
 # install packages
 sudo apt-get install vim gcc g++ clang tmux git ripgrep bat exa  -y
 
 # install neovim build prerequisites
-sudo apt-get install ninja-build gettext cmake unzip curl -y
+sudo apt-get install ninja-build gettext cmake unzip curl git -y
 
-# install neovim
+
+echo "download dotfiles"
+git clone https://github.com/YukiYuigishi/dotfiles.git ~/.dotfiles
+
+echo "install neovim"
 cd /tmp
 git clone -b stable --depth=1 https://github.com/neovim/neovim.git
 cd neovim
@@ -17,6 +20,7 @@ git checkout stable
 make CMAKE_BUILD_TYPE=Release -j
 sudo make install 
 
+echo "migrate dotfiles"
 mkdir -p $HOME/.config
 cd $HOME/.config
 ln -s $HOME/.dotfiles/code code
@@ -33,5 +37,6 @@ ln -s $HOME/.dotfiles/zsh zsh
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim -v
-nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 
+nvim --headless -c 'TSUpdate'
 

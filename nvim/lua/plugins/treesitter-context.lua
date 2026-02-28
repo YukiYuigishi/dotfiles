30028ae83432e3f012272e8c63379dc860544c73
Yuki Yuigishi <me@yuigishi.dev>
2026-02-28 21:34:14 +0900

    chore: add hexinspector

--- nvim/lua/plugins/treesitter-context.lua
+++ nvim/lua/plugins/treesitter-context.lua
@@ -16,4 +16,5 @@ return {
         zindex = 20,     -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
     },
+    enabled = true,
 }
